cfc0ab0450b3c9aa8dca22e1db526b1d8167987e
Ruixin Li <kazezakimiyu@gmail.com>
2024-04-08 21:00:01 -0400

    update lstm model

---
+++ LSTM.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import re
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('./sentiment_data.csv', encoding='ISO-8859-1', header=None)
+df.columns = ['target', 'ids', 'date', 'flag', 'user', 'text']
+
+df['text_clean'] = df['text'].apply(lambda x: ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", x).split()))
+
+tokenizer = Tokenizer(num_words=5000, oov_token="<OOV>")
+tokenizer.fit_on_texts(df['text_clean'])
+sequences = tokenizer.texts_to_sequences(df['text_clean'])
+
+max_length = max([len(x) for x in sequences])
+padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
+
+df['target'] = df['target'].map({0: 0, 2: 1, 4: 2}) 
+targets = to_categorical(df['target'], num_classes=3)
+
+X_train, X_test, y_train, y_test = train_test_split(padded_sequences, targets, test_size=0.2, random_state=42)
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
+
+model = Sequential([
+    Embedding(input_dim=5000, output_dim=64, input_length=max_length),
+    SpatialDropout1D(0.2),
+    LSTM(100, dropout=0.2, recurrent_dropout=0.2),
+    Dense(3, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
+
+test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+print('\nTest accuracy:', test_acc)
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+download dataset from: https://www.kaggle.com/datasets/kazanova/sentiment140?resource=download
