0be8644dc454c7da8825023359b665ee7d1d2ac4
Ruixin Li <kazezakimiyu@gmail.com>
2024-04-13 23:15:20 -0700

    fix bug for lstm

--- LSTM.py
+++ LSTM.py
@@ -23,7 +23,11 @@ sequences = tokenizer.texts_to_sequences(df['text_clean'])
 max_length = max([len(x) for x in sequences])
 padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
 
-df['target'] = df['target'].map({0: 0, 2: 1, 4: 2}) 
+print("Unique target labels before mapping:", df[0].unique()) #suppose to return 3
+
+df['target'] = df['target'].map({0: 0, 2: 1, 4: 2})
+print("Unique target labels after mapping:", df['target'].unique())
+
 targets = to_categorical(df['target'], num_classes=3)
 
 X_train, X_test, y_train, y_test = train_test_split(padded_sequences, targets, test_size=0.2, random_state=42)
@@ -45,14 +49,16 @@ test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
 print('\nTest accuracy:', test_acc)
 
 # Convert the probabilities to class labels
-y_pred = model.predict(X_test)
-y_pred = np.argmax(y_pred, axis=1)
+y_pred = np.argmax(model.predict(X_test), axis=1)
 y_true = np.argmax(y_test, axis=1)
 
 # Generate classification report
-report = classification_report(y_true, y_pred, target_names=['Negative', 'Neutral', 'Positive'], output_dict=True)
+unique_classes = np.unique(y_true)
+class_names = ['Negative', 'Neutral', 'Positive']
+active_class_names = [class_names[i] for i in unique_classes]
+report = classification_report(y_true, y_pred, target_names=active_class_names, output_dict=True)
 print('\nTest accuracy:', test_acc)
-print('\nClassification Report:\n', classification_report(y_true, y_pred, target_names=['Negative', 'Neutral', 'Positive']))
+print('\nClassification Report:\n', classification_report(y_true, y_pred, target_names=active_class_names))
 
 # Save the LSTM model
 model.save('sentiment_lstm_model.h5')
@@ -72,7 +78,7 @@ with open('lstm_metrics.txt', 'w') as f:
     for key, value in metrics.items():
         f.write(f'{key}: {value}\n')
 '''
-report = classification_report(y_true, y_pred, target_names=['Negative', 'Neutral', 'Positive'], output_dict=True)
+
 # Save the report as JSON
 with open('lstm_classification_report.json', 'w') as f:
     json.dump(report, f)
